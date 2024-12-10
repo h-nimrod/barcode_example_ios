036bdc627f7133ee4111dc0f2cf3a00d6e974bd8
Nimrod Project <h.nimrod.jp@gmail.com>
2024-12-10 19:50:00 +0900

    Initial Commit

---
+++ barcode_example_ios/barcode_example_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  barcode_example_iosApp.swift
+//  barcode_example_ios
+//
+//  Created by h-nimrod on 2024/12/10.
+//
+
+import SwiftUI
+
+@main
+struct barcode_example_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
